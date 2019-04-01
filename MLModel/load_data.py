2b02a96146ffa0f874e78e4a601555850e447566
Darian Gratton <Dariangratton@gmail.com>
2019-03-31 17:02:02 -0700

    - Testing with SVC - Created function to get missing features when hot encoding'

--- MLModel/load_data.py
+++ MLModel/load_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 def load_full_data():
     train_data = pd.read_csv("./trainingset.csv")
@@ -6,7 +8,8 @@ def load_full_data():
 
     return pd.concat([train_data, test_data], axis=0)
 
-def load(file_path, one_hot_encode = False, max_unique = 20):
+
+def load(file_path, one_hot_encode=False, max_unique=20):
     """
     Load a .csv file
     :param file_path: path to .csv
@@ -22,10 +25,37 @@ def load(file_path, one_hot_encode = False, max_unique = 20):
     if one_hot_encode:
         hot_encoding_indices = []
         for col in data:
-            if 2 < data[col].nunique() < max_unique:
+            if 1 < data[col].nunique() < max_unique and str(col) != 'income':
                 hot_encoding_indices.append(col)
         data = pd.get_dummies(data, columns=hot_encoding_indices, prefix=hot_encoding_indices)
 
     return data
 
 
+def get_missing_features(train_data, test_data):
+    """
+    Makes the test set features the same as the training set features
+    :param train_data: the training set data
+    :param test_data: the testing set data
+    :return: The test data with the features from the training data (Not Sorted)
+    """
+    # Drop income from train_data if income exists
+    if 'income' in train_data:
+        train_data = train_data.drop(['income'], axis=1, inplace=False)
+
+    # Get features in each list
+    train_features = list(train_data)
+    test_features = list(test_data)
+
+    # Get features that are in one but not the other
+    missing_features = np.setdiff1d(train_features, test_features)
+    not_needed_features = np.setdiff1d(test_features, train_features)
+
+    # Drop columns in the testset not in the training set
+    new_data = test_data.drop(not_needed_features, axis=1, inplace=False)
+
+    # Add columns into the testset that are in the training set but not the test set
+    for feature in missing_features:
+        new_data[feature] = 0
+
+    return new_data
--- MLModel/main_model.py
+++ MLModel/main_model.py
@@ -5,8 +5,8 @@ import random
 from sklearn.svm import SVC
 
 # Load the training and testing data
-train_data = loader.load('./trainingset.csv')
-test_data = loader.load('./testingset.csv')
+train_data = loader.load('./trainingset.csv', one_hot_encode=True, max_unique=50)
+test_data = loader.load('./testingset.csv', one_hot_encode=True, max_unique=50)
 
 # Number of samples in the train_data
 num_rows = train_data.shape[0]
@@ -26,3 +26,21 @@ train_features = train_data.drop('income', axis=1, inplace=False)
 train_labels = train_data.loc[:, ['income']]
 test_features = test_data.drop('income', axis=1, inplace=False)
 test_labels = test_data.loc[:, ['income']]
+
+test_features = loader.get_missing_features(train_features, test_features)
+
+clf = SVC(gamma='auto')
+clf.fit(train_features, train_labels)
+pred = clf.predict(test_features)
+
+# Check how many are correctly predicted
+j = 0
+correct_pred = 0
+for predicted in pred:
+    validation_labels_val = test_labels.iloc[j]
+    if predicted == validation_labels_val.income:
+        correct_pred += 1
+    j += 1
+
+error_rate = (len(test_data) - correct_pred) / len(test_data)
+print("Test Set Error Rate: ", error_rate)
