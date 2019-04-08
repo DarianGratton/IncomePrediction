8c618430e8a8481fdcae1538ac91e352388d0dfa
Darian Gratton <Dariangratton@gmail.com>
2019-04-08 01:01:43 -0700

    Add a way to Parse JSON from a request in the api

--- PredictionService/Predictions/views.py
+++ PredictionService/Predictions/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import JSONParser
+import pickle
+
 
 # Create your views here.
 class PredictionView(APIView):
     permission_classes = (IsAuthenticated,)
+    parser_classes = (JSONParser,)
 
     def post(self, request):
-        prediction = 'placeholder'
-        content = {'prediction': prediction}
+        prediction = predict_income(request.data)
+        print(request)
+        content = {'prediction': request.data}
         return Response(content)
